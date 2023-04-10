1c69df94e60341e58e85ff999a29dad6cc511fc8
MuhammadNuman24 <numanrazzaq24@gmail.com>
2023-04-10 21:15:48 +0000

    basic routing in express

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app= express();
 
+const port = 8000;
 app.get("/", (req, res)=>{
     res.send("Hello World From Express");
 })
-app.listen(8000 ,()=>{
-     console.log("Listining  on port 8000");
+app.get("/about", (req, res)=>{
+    res.status(200), res.send("Wel Come from About Page ");
+})
+app.listen(port,()=>{
+    console.log (`Listening  on port ${port}`);
 })
