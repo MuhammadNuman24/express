6b69969322f0630e3822cb5824cf5fa03f7d3488
MuhammadNuman24 <numanrazzaq24@gmail.com>
2023-04-10 21:42:35 +0000

    static website rendering

--- src/index.js
+++ src/index.js
@@ -1,25 +1,17 @@
+const { log } = require("console");
 const express = require("express");
+const path = require("path");
 const app= express();
-
+// default middlevier for 
 const port = 8000;
+const staticpath= path.join(__dirname ,"../public");
+console.log(path.join(__dirname ,"../public"));
+app.use(express.static(staticpath))
 app.get("/", (req, res)=>{
     res.send("<h1>Hello World From Express</h1>"); // html responce 
 })
 app.get("/about", (req, res)=>{
-    res.send([
-        {
-            id:01,
-            name:"Numan"
-        },
-        {
-            id:02,
-            name:"Ali"
-        },
-        {
-            id:03,
-            name:"Sufyan"
-        }
-    ]);
+    
 })
 app.listen(port,()=>{
     console.log (`Listening  on port ${port}`);
